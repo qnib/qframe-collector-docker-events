8f7dcbde5bbe44f3e1cec821d760dda4f0e61f9d
Christian Kniep <christian@qnib.org>
2017-04-21 15:40:21 +0200

    initial commit with working plugin

---
+++ lib/plugin.go
@@ -0,0 +1,68 @@
+package qframe_collector_docker_events
+
+import (
+	"fmt"
+	"log"
+	"github.com/docker/docker/client"
+	"golang.org/x/net/context"
+	"github.com/docker/docker/api/types"
+	"github.com/zpatrick/go-config"
+
+	"github.com/qnib/qframe-types"
+)
+
+const (
+	version = "0.0.0"
+	pluginTyp = "collector"
+	dockerAPI = "v1.29"
+)
+
+var (
+	SuperMap = make(map[string]ContainerSupervisor)
+)
+
+
+type Plugin struct {
+	qtypes.Plugin
+	engCli *client.Client
+}
+
+func New(qChan qtypes.QChan, cfg config.Config, name string) (Plugin, error) {
+	var err error
+	p := Plugin{
+		Plugin: qtypes.NewNamedPlugin(qChan, cfg, pluginTyp, name, version),
+	}
+	return p, err
+}
+
+func (p *Plugin) Run() {
+	dockerHost := p.CfgStringOr("docker-host", "unix:///var/run/docker.sock")
+	// Filter start/stop event of a container
+	engineCli, err := client.NewClient(dockerHost, dockerAPI, nil, nil)
+	if err != nil {
+		p.Log("error", fmt.Sprintf("Could not connect docker/docker/client to '%s': %v", dockerHost, err))
+		return
+	}
+	info, err := engineCli.Info(context.Background())
+	if err != nil {
+		p.Log("error", fmt.Sprintf("Error during Info(): %v >err> %s", info, err))
+		return
+	} else {
+		p.Log("info", fmt.Sprintf("Connected to '%s' / v'%s'", info.Name, info.ServerVersion))
+	}
+
+	msgs, errs := engineCli.Events(context.Background(), types.EventsOptions{})
+	for {
+		select {
+		case dMsg := <-msgs:
+			qm := qtypes.NewQMsg("docker-event", "docker-events")
+			qm.Msg = fmt.Sprintf("%s: %s.%s", dMsg.Actor.Attributes["name"], dMsg.Type, dMsg.Action)
+			qm.Data = dMsg
+			p.QChan.Data.Send(qm)
+		case dErr := <-errs:
+			if dErr != nil {
+				log.Printf("[EE] %v", dErr)
+			}
+		}
+	}
+}
---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"log"
+	"fmt"
+	"time"
+
+	"github.com/zpatrick/go-config"
+	"github.com/qnib/qframe-types"
+	"github.com/qnib/qframe-collector-docker-events/lib"
+)
+
+func Run(qChan qtypes.QChan, cfg config.Config, name string) {
+	p, _ := qframe_collector_docker_events.New(qChan, cfg, name)
+	p.Run()
+}
+
+func main() {
+	qChan := qtypes.NewQChan()
+	qChan.Broadcast()
+	cfgMap := map[string]string{
+		"collector.test.port": "10001",
+	}
+
+	cfg := config.NewConfig(
+		[]config.Provider{
+			config.NewStatic(cfgMap),
+		},
+	)
+	p, err := qframe_collector_docker_events.New(qChan, *cfg, "test")
+	if err != nil {
+		log.Printf("[EE] Failed to create collector: %v", err)
+		return
+	}
+	go p.Run()
+	time.Sleep(2*time.Second)
+	bg := qChan.Data.Join()
+	for {
+		qm := bg.Recv().(qtypes.QMsg)
+		fmt.Printf("#### Received: %s\n", qm.Msg)
+		break
+	}
+}
