5a6a9c9f48239d96007ccdcebfce96f74292607d
Christian Kniep <christian@qnib.org>
2017-04-22 15:30:43 +0200

    move back to docker/docker

--- lib/plugin.go
+++ lib/plugin.go
@@ -3,9 +3,9 @@ package qframe_collector_docker_events
 import (
 	"fmt"
 	"log"
-	"github.com/moby/moby/client"
+	"github.com/docker/docker/client"
 	"golang.org/x/net/context"
-	"github.com/moby/moby/api/types"
+	"github.com/docker/docker/api/types"
 	"github.com/zpatrick/go-config"
 
 	"github.com/qnib/qframe-types"
